d331eff6fcd002c308056bb709f981c66ed39018
Steliana-Guta <steliana.m.guta@gmail.com>
2024-05-27 19:37:16 +0100

    Add project files.

---
+++ BusinessCard/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BusinessCard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            Console.WriteLine("Please enter your name");
+           string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name + ",can you please insert your phone number?");
+           string phone = Console.ReadLine();
+            Console.WriteLine("Thank you, and where are you working or studying at the moment?");
+           string work = Console.ReadLine();
+            Console.WriteLine("Informations gathered:");
+            Console.WriteLine(work);
+            Console.WriteLine(name);
+            Console.WriteLine(phone);
+            Console.ReadLine();
+
+        }
+    }
+}
